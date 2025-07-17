30447cb6fb03ba268f9cfb73d7d1484f1aec01dd
udayanagani <udayanganishiranthika@gmail.com>
2025-07-17 17:08:44 +0530

    first commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\UserLoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    public function login(UserLoginRequest $request)
+    {
+        $credentials = $request->validated();
+
+        $user = User::where('name', $credentials['name'])->first();
+
+        // if (!$user->availability) {
+        //     return response()->json(['message' => 'Your account is Unavailabile.'], 403);
+        // }
+
+        // if (!$user || !Hash::check($credentials['password'], $user->password)) {
+        //     return response()->json([
+        //         'message' => 'Invalid credentials!',
+        //     ], 401);
+        // }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login successful!',
+            'token' => $token,
+        ], 201);
+    }
+}
