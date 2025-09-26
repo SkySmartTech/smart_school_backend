6e6538859fcb8b3b43d850c0577ef1a3a0b40998
udayanagani <udayanganishiranthika@gmail.com>
2025-09-26 23:32:29 +0530

    class students page added

---
+++ app/Http/Requests/User/StudentsGradeUpdateRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StudentsGradeUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'students' => 'required|array',
+            'students.*.name' => 'nullable|string|max:255',
+            'students.*.studentAdmissionNo' => 'required|string|max:255',
+            'students.*.year' => 'nullable|digits:4|integer',
+            'students.*.studentGrade' => 'required|string|max:255',
+            'students.*.studentClass' => 'required|string|max:255',
+        ];
+    }
+}
--- app/Models/UserStudent.php
+++ app/Models/UserStudent.php
@@ -14,6 +14,7 @@ class UserStudent extends Model
         'medium',
         'studentClass',
         'studentAdmissionNo',
+        'year',
         'parentNo',
         'parentProfession',
         'userType',
